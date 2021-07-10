ad771aad441c2bc879c8ebea5cf147484c3c8057
Yasin <yasinm289@gmail.com>
2021-07-10 14:29:02 +0430

    horizontal and vertical list added

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -13,10 +13,37 @@ class _HomePageState extends State<HomePage> {
         title: Text("Horizontal & Vertical ListView"),
         backgroundColor: Colors.green[700],
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) => ListTile(
-          title: Text("Okay"),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => Container(
+                  height: 100,
+                  margin: EdgeInsets.all(10),
+                  width: 100,
+                  child: Center(
+                    child: Text("Card $index"),
+                  ),
+                  color: Colors.green,
+                ),
+              ),
+            ),
+            Flexible(
+              child: ListView.builder(
+                itemCount: 15,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text("List $index"),
+                ),
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+              ),
+            ),
+          ],
         ),
       ),
     );
